2fba209867acdf475671a706d77875b000197039
DieterDraelants <dieter.draelants@student.ucll.be>
2019-10-02 16:42:46 +0200

    damaged state done

--- src/domain/PartyArticle/DamagedState.java
+++ src/domain/PartyArticle/DamagedState.java
@@ -9,21 +9,23 @@ public class DamagedState implements IState {
 
     @Override
     public void Delete() {
-
+        System.out.println("je verwijdert je product");
+        partyArticle.setState(partyArticle.getDeletedState());
     }
 
     @Override
     public void Lend() {
-
+        System.out.println("je kan het niet lenen wanneer het beschadigd is");
     }
 
     @Override
     public void Return(Boolean b) {
-
+        System.out.println("je kan niet terugbrengen want het is al terug");
     }
 
     @Override
     public void Repair() {
-
+        System.out.println("je repareert");
+        partyArticle.setState(partyArticle.getLendableState());
     }
 }
